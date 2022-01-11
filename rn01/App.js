a365e0ff8aadfe34614ae658a5d12d0799377547
unknown <devcoderian@gmail.com>
2022-01-11 11:35:32 +0900

    TextInput 예제

--- rn01/App.js
+++ rn01/App.js
@@ -1,35 +1,52 @@
 import React, {Component} from 'react'
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Button, TextInput} from 'react-native'
 import Input from './src/input'
 
 class App extends Component{
 
-  state= {
-    appName: 'My First App',
-    random : [242, 23]
-  }
-
-  onAddRandomNum = () => {
-    const randomNum = Math.floor(Math.random()*100)+1;
-    this.setState(prevState =>{
+  onAddTextInput = () => {
+    this.setState(prevState => {
       return {
-        random: [...prevState.random, randomNum]
+        myTextInput: '',
+        alphabet: [...prevState.alphabet, prevState.myTextInput]
       }
     })
-  } 
-
-  onNumDelete = (position) => {
-    const newArray = this.state.random.filter((num, idx) => {
-      return position != idx;
-    })
-    this.setState({
-      random: newArray
-    })
   }
+
+  state= {
+    myTextInput: '',
+    alphabet: ['a', 'b', 'c', 'd']
+   }
+ 
+   onChangeInput = (e) => {
+     this.setState({
+       myTextInput: e
+     });
+   }
+  
   render(){
     return (
       <View style ={styles.mainView}>
-        <Input />
+         <TextInput
+            value={this.state.myTextInput}
+            style={styles.input}
+            onChangeText={this.onChangeInput}
+            //multiline={true} //개행 기능
+            maxLength={10} //글자수제한
+            //autoCapitalize={'none'} //대문자 자동수정 끄기
+            //editable={false} //text-input disabled 
+        
+        />
+        <Button
+          title="Add Text Input"
+          onPress={() => this.onAddTextInput()}
+        />
+        <ScrollView style={{width: '100%'}}>
+          {
+          this.state.alphabet.map((item, idx)=> (
+              <Text style={styles.mainText} key={idx}>{item}</Text>
+          ))}
+        </ScrollView>
       </View>
     )
   }
@@ -59,7 +76,16 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'normal',
     color: 'black',
-    padding: 20
+    padding: 20,
+    margin: 20,
+    backgroundColor: 'pink'
+  },
+  input:{
+    width:'100%',
+    backgroundColor: '#cecece',
+    marginTop: 20,
+    fontSize: 25,
+    padding: 10
   }
 })
 //position default 왼쪽 위
